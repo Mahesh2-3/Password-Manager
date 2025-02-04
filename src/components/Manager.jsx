a4a1d2a612bc910e49b70550a36cf866affa8880
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-02-04 09:47:26 +0530

    Update Manager.jsx

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -64,7 +64,7 @@ const Manager = () => {
                 theme="dark"
             />
 
-            <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
+            <div className="absolute inset-0 -z-10 h-[80vh] w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
 
             <div className=" p-3 container min-h-[80vh] m-auto">
                 <div className='flex flex-col gap-2 justify-center items-center w-[100%]'>
