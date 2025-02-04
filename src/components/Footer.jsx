5fb6341aa947e8e387a201a3e4ef7f11d34b629d
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-02-04 09:46:53 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <div className='bottom-0 sticky w-full flex flex-col items-center justify-evenly  bg-green-950 h-[10vh]'>
+      <div className='bottom-0 sticky w-full flex flex-col items-center justify-evenly  bg-green-950 '>
         <div className=' flex flex-row gap-2  items-center justify-center'>
         <div className="logo text-2xl font-bold text-green-500">
           &lt;<span className="text-white">Pass</span><span>OP</span>/&gt;
@@ -13,8 +13,8 @@ const Footer = () => {
         </div>
           </div>
         <div className="text-sm text-center text-white py-3">
-          "This website is fully functional and designed to provide a complete experience. However, due to Google’s restrictions, we have temporarily disabled user data collection. You can explore all features without signing in or providing personal information."
-        </div>
+
+"This website is fully functional, but user data collection is disabled due to Google’s restrictions. Enjoy exploring!"        </div>
       </div>
     </>
   )
